4f11f4586a2449f6a24df013b4a0fde7ae584369
CiaranSlattery <ciaranslattery@gmail.com>
2021-11-23 10:09:45 +0000

    Creating React Application in IntelliJ Backend

---
+++ src/main/webapp/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './bitcoin-btc-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Welcome to Coin A-Z
+        </p>
+    </div>
+  );
+}
+
+export default App;
