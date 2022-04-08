8550804cdf05c66894501fe7056501b10f681b4e
CiaranSlattery <ciaranslattery@gmail.com>
2022-04-08 10:09:45 +0100

    Reverting to an old commit

--- BackendApp/src/main/java/az/coin/backendapp/helloWorld/BasicAuthenticationController.java
+++ BackendApp/src/main/java/az/coin/backendapp/helloWorld/BasicAuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://54.247.123.110/:3000")
 public class BasicAuthenticationController {
     //bean
     @GetMapping(path="/basicauth")
