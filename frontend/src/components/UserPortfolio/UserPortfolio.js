a6b4ffc167865439b91bb073cfa4cf2e9a0539ce
CiaranSlattery <ciaranslattery@gmail.com>
2022-04-12 21:51:42 +0100

    Package.json config

--- frontend/src/components/UserPortfolio/UserPortfolio.js
+++ frontend/src/components/UserPortfolio/UserPortfolio.js
@@ -1,46 +1,48 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import mockdata from "../../mockdata.json";
 import deleteAssetClicked from "../PortfolioList/PortfolioList";
 import AuthenticationService from "../User/AuthenticationService";
 import PortfolioDataService from "../portfolio/PortfolioDataService";
 
-const UserPortfolio = ({coins, ciaranCoin}) => {
+const UserPortfolio = ({ coins, ciaranCoin }) => {
   const [fetchedData, setFetchedData] = useState([]);
 
-  
   const refreshPortfolio = async () => {
     let username = AuthenticationService.getLoggedInUserName();
     try {
-      const response = await PortfolioDataService.retrieveAllPortfolio(username);
+      const response = await PortfolioDataService.retrieveAllPortfolio(
+        username
+      );
       setFetchedData(response.data);
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
   };
-  
+
   const deleteAssetClicked = async (id) => {
     let username = AuthenticationService.getLoggedInUserName();
     try {
       await PortfolioDataService.deleteAssetPortfolio(username, id);
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-      refreshPortfolio();
+    refreshPortfolio();
   };
 
   useEffect(() => {
-    refreshPortfolio()
-  }, [ciaranCoin])
+    refreshPortfolio();
+  }, [ciaranCoin]);
 
   const totalValue = () => {
     let totalSum = 0;
     fetchedData?.forEach((coin) => {
-      totalSum += Math.round(coins.find((x) => x.id === coin.id)?.current_price * coin.quantity)}) 
-    return totalSum  
-  }
-      
-  
+      totalSum += Math.round(
+        coins.find((x) => x.id === coin.id)?.current_price * coin.quantity
+      );
+    });
+    return totalSum;
+  };
 
   return (
     <>
@@ -52,18 +54,30 @@ const UserPortfolio = ({coins, ciaranCoin}) => {
       </div>
       <div className="portfolio-coins">
         {fetchedData?.map((coin) => (
-          <div className="list-item" >
+          <div className="list-item">
             <p>{coin.id}</p>
             <p>{coin.quantity}</p>
-            <p>$ {Math.round(coins.find((x) => x.id === coin.id)?.current_price * coin.quantity)}</p>      
-            <p><button className="btn btn-warning" onClick={() => deleteAssetClicked(coin.id)}>Delete</button></p>      
+            <p>
+              ${" "}
+              {Math.round(
+                coins.find((x) => x.id === coin.id)?.current_price *
+                  coin.quantity
+              )}
+            </p>
+            <p>
+              <button
+                className="btn btn-warning"
+                onClick={() => deleteAssetClicked(coin.id)}
+              >
+                Delete
+              </button>
+            </p>
           </div>
         ))}
       </div>
       <div className="net-value">
         <p>Net Value: $ {totalValue()}</p>
       </div>
-
     </>
   );
 };
